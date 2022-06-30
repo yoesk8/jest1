0d47dea3393f56603074cb85c335075aa274d498
yoesk8 <yoelgutierrez2021@yahoo.com>
2022-06-30 10:16:05 +0000

    test improved

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,5 +1,5 @@
-function addition(){
-    return 42;
+function addition(a,b){
+    return a + b;
 }
 
 module.exports = addition;
--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -5,7 +5,10 @@ describe("Calculator", ()=> {
     describe("Addition function", ()=> {
         test("Should return 42 for 20 + 22", ()=> {
             expect(addition(20,22)).toBe(42);
-        })
+        });
+        test("Should return 20 for 10 + 10", ()=> {
+            expect(addition(10,10)).toBe(20);
+        });
     });
     describe("Subtraction function", ()=> {
 
